8a5e5b25c0cbac1e3cb6ad63a27d6351eaa29108
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-26 18:13:55 +0100

    Added mod items class

---
+++ src/main/java/singh/akaalroop/winter_of_making/ModItems.java
@@ -0,0 +1,28 @@
+package singh.akaalroop.winter_of_making;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+import static singh.akaalroop.winter_of_making.WinterOfMaking.MOD_ID;
+
+public class ModItems {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+}
