519df1d1d5ae705ac65e1720eef356a9838816fb
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-26 18:02:17 +0100

    Initial commit

---
+++ src/client/java/singh/akaalroop/winter_of_making/WinterOfMakingClient.java
@@ -0,0 +1,10 @@
+package singh.akaalroop.winter_of_making;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WinterOfMakingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/singh/akaalroop/winter_of_making/WinterOfMakingDataGenerator.java
@@ -0,0 +1,11 @@
+package singh.akaalroop.winter_of_making;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WinterOfMakingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
