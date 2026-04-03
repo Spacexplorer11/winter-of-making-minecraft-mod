c2775be3179f5240d20be46bff3d80d197502ef4
Akaalroop Singh <akaal@akaalroop.com>
2026-04-03 15:35:29 +0100

    Add conditions to Client code so versions >= 1.21.9 don't use the deprecated API
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/client/java/singh/akaalroop/winter_of_making/WinterOfMakingClient.java
+++ src/client/java/singh/akaalroop/winter_of_making/WinterOfMakingClient.java
@@ -1,17 +1,28 @@
 package singh.akaalroop.winter_of_making;
 
 import net.fabricmc.api.ClientModInitializer;
+        //? if >= 1.21.9 {
+/*import net.minecraft.client.render.entity.EntityRendererFactories;
+ *///? } else {
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+//? }
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import singh.akaalroop.winter_of_making.entities.ModEntities;
 
 public class WinterOfMakingClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        //? if >= 1.21.9 {
+        /*EntityRendererFactories.register(
+                ModEntities.KNOCKBACK_SNOWBALL,
+                FlyingItemEntityRenderer::new
+        );
+        *///? } else {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
         EntityRendererRegistry.register(
                 ModEntities.KNOCKBACK_SNOWBALL,
                 FlyingItemEntityRenderer::new
         );
+        //? }
     }
 }
